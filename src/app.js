93dc1746598978e08d31e1118039d523d0183cb2
Agnieszka Wojsznis <agnieszka.wojsznis@protonmail.com>
2019-01-18 16:41:03 +0100

    Setting up 404

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 const Test = () => (
     <p> Hello routing </p> 
@@ -20,14 +20,19 @@ const Help = ()=> (
     <p> Help page </p>
 )
 
+const NoFound = ()=> (
+    <p> 404 page </p>
+)
+
 const routing = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path='/' component={Test} exact={true} />
             <Route path='/entry' component={Entry} />
             <Route path='/help' component={Help} />
             <Route path='/edit' component={Edit} />
-        </div>
+            <Route component={NoFound} />
+        </Switch>
     </BrowserRouter>
 )
 
