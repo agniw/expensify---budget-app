11d498915e602efb0e91791665b8336018133128
Agnieszka Wojsznis <agnieszka.wojsznis@protonmail.com>
2019-01-19 18:13:04 +0100

    Linking between components with Link

--- src/app.js
+++ src/app.js
@@ -2,37 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 
 const Test = () => (
     <p> Hello routing </p> 
 )
 
-const Entry = ()=> (
+const Entry = () => (
     <p> Entry </p>
 )
 
-const Edit = ()=> (
+const Edit = () => (
     <p> Edit page </p>
 )
 
-const Help = ()=> (
+const Help = () => (
     <p> Help page </p>
 )
 
-const NoFound = ()=> (
-    <p> 404 page </p>
+const NoFound = () => (
+    <div>
+        <p> 404 page </p> 
+        <Link to='/'> Go to </Link>
+    </div>
+)
+
+const Header = () => (
+    <div>
+        <p> Expensify </p>
+        <Link to='/'> Home </Link>
+        <Link to='/entry'> Entry </Link>
+        <Link to='/edit'> Edit </Link>
+        <Link to='/help'> Help </Link>
+    </div>
 )
 
 const routing = (
     <BrowserRouter>
-        <Switch>
-            <Route path='/' component={Test} exact={true} />
-            <Route path='/entry' component={Entry} />
-            <Route path='/help' component={Help} />
-            <Route path='/edit' component={Edit} />
-            <Route component={NoFound} />
-        </Switch>
+        <div>
+            <Header />
+            <Switch>
+                <Route path='/' component={Test} exact={true} />
+                <Route path='/entry' component={Entry} />
+                <Route path='/help' component={Help} />
+                <Route path='/edit' component={Edit} />
+                <Route component={NoFound} />
+            </Switch>
+        </div>
     </BrowserRouter>
 )
 
